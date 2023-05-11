3fbd02ce7f1b3f679fc7b4ed739645386806f729
Gabriel Gore <gabriel59kg@gmail.com>
2023-05-11 14:49:32 -0400

    fn gcd

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -52,3 +52,16 @@ fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
 
     HttpResponse::Ok().content_type("text/html").body(response)
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n
+    }
+    n
+}
